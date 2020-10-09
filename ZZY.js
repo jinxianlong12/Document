4bce3288cee4fca21fbed21b1b6cde3518588b05
szweixun <jinxianlong@szweixun.com.cn>
2020-10-09 14:40:09 +0800

    1
    111

--- ZZY.js
+++ ZZY.js
@@ -1,46 +1,36 @@
-﻿var totalWaitPcs = 0
-var totalTurn = 0
-var totalCount = 0
-var totaldone = 0
-if (value) {
-  var yesNoArr = ['Y', 'N']
-  for (var i = 27; i <= 34; i++) {
-    var target = this.root.findById('M' + i)
-    var rand = yesNoArr[Math.floor(Math.random() * yesNoArr.length)];
-    var waitingPCS = Math.floor(Math.random() * 117);
-    totalWaitPcs += waitingPCS
-    if (value[i - 27] > value[50 - i]) {
-      currentCnt = value[50 - i] * 61
-      totalCnt = value[i - 27] * 61
-      occRate = (value[50 - i] / value[i - 27]) * 100
-      totalCount += totalCnt
-      totaldone += currentCnt
-    } else {
-      currentCnt = value[i - 27] * 61
-      totalCnt = value[50 - i] * 61
-      occRate = (value[i - 27] / value[50 - i]) * 100
-      totalCount += totalCnt
-      totaldone += currentCnt
-    }
-    var makeObj = {
-      "enable": rand,
-      "occRate": occRate,
-      "waitingPcs": waitingPCS,
-      "cycleRate": value[80 - i],
-      "currentCnt": currentCnt,
-      "totalCnt": totalCnt
-    }
-    target.data = makeObj;
-    totalTurn += value[80 - i]
+﻿if (this._timeout)
+  clearTimeout(this._timeout);
+
+if (!value.length)
+  return;
+
+// Please Setting Value
+const cellCount = 3;
+const time = 2000;
+const tableId = 'table';
+const nullObj = {
+  index: "",
+  data: ""
+};
+
+const totalCount = Math.floor(((value.length - 1) / cellCount) + 1);
+const remainder = value.length % cellCount;
+
+pagingData(value, 0, totalCount, remainder, cellCount, time, nullObj, tableId, this);
+
+function pagingData(list, index, totalCount, remainder, cellCount, time, nullObj, tableId, self) {
+  var shift = index * cellCount;
+  var data;
+
+  if (index + 1 == totalCount) {
+    data = list.slice(shift);
+    for (let i = 0; i < cellCount - remainder; i++)
+      data.push(nullObj);
+    index = 0;
+  } else {
+    data = list.slice(shift, cellCount + shift);
+    index++;
   }
-  var workWaitTarget = this.root.findById('totalWaitingPcs')
-  workWaitTarget.data = totalWaitPcs
-  var cellTurnTarget = this.root.findById('totalCycleRate')
-  cellTurnTarget.data = totalTurn / 8
-  var totalTarget = this.root.findById('totalTotal')
-  totalTarget.data = totalCount
-  var totalCntTarget = this.root.findById('totalCount')
-  totalCntTarget.data = totaldone
-  var totalOccTarget = this.root.findById('totalOccRate')
-  totalOccTarget.data = (totaldone / totalCount) * 100
+  self.parent.findById(tableId).data = data
+  self._timeout = setTimeout(function () { pagingData(list, index, totalCount, remainder, cellCount, time, nullObj, tableId, self) }, time);
 }
